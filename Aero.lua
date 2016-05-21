f829aaddd36b942604ed767d1b724866ee8d7b14
gashole <gashole@users.noreply.github.com>
2016-05-20 18:00:01 -0700

    Initial commit

---
+++ Aero.lua
@@ -0,0 +1,110 @@
+﻿local _G = getfenv(0)
+local Aero = CreateFrame"Frame"
+local aero, percent, value
+local running = {}
+local next = next
+
+local duration = 0.15
+
+local function print(msg)
+	DEFAULT_CHAT_FRAME:AddMessage(msg)
+end
+
+local function Anim(frame)
+	aero = frame.aero
+	percent = aero.total / duration
+	value = aero.start + aero.diff * percent
+	if value <= 0 then value = 0.01 end
+	frame:SetAlpha(value)
+	frame:SetScale(value)
+end
+
+local function OnFinish(frame)
+	frame:SetScale(1)
+	frame:SetAlpha(1)
+	if frame.onfinishhide then
+		frame.onfinishhide = nil
+		HideUIPanel(frame)
+		frame.hiding = nil
+	end
+end
+
+local function OnUpdate()
+	for i, frame in next, running do
+		aero = frame.aero
+		aero.total = aero.total + arg1
+		if aero.total >= duration then
+			aero.total = 0
+			running[i] = nil
+			OnFinish(frame)
+		else
+			Anim(frame)
+		end
+	end
+end
+Aero:SetScript("OnUpdate", OnUpdate)
+
+local function OnShow()
+	this.onshow()
+	if this.hiding or running[this] or UnitAffectingCombat"player" then return end
+	tinsert(running, this)
+	this.aero.start = 0.5
+	this.aero.diff = 0.5
+end
+
+local function OnHide()
+	this.onhide()
+	if this.hiding or running[this] or UnitAffectingCombat"player" then return end
+	tinsert(running, this)
+	this.aero.start = 1
+	this.aero.diff = -0.5
+	this.onfinishhide = true
+	this.hiding = true
+	this:Show()
+end
+
+function Aero:RegisterFrames(...)
+	for i = 1, arg.n do
+		local arg = arg[i]
+		if type(arg) == "string" then
+			local frame = _G[arg]
+			if not frame then return print("Aero:|cff98F5FF "..arg.."|r not found.") end
+			frame.aero = frame.aero or {}
+			frame.aero.total = 0
+			frame.onshow = frame:GetScript("OnShow") or function() end
+			frame.onhide = frame:GetScript("OnHide") or function() end
+			frame:SetScript("OnShow", OnShow)
+			frame:SetScript("OnHide", OnHide)
+		else
+			arg()
+		end
+	end
+end
+
+local addons = {}
+local function OnEvent()
+	if addons[arg1] then
+		Aero:RegisterFrames(unpack(addons[arg1]))
+		addons[arg1] = nil
+	end
+end
+Aero:RegisterEvent"ADDON_LOADED"
+Aero:SetScript("OnEvent", OnEvent)
+
+function Aero:RegisterAddon(addon, ...)
+	if IsAddOnLoaded(addon) then
+		for i = 1, arg.n do
+			Aero:RegisterFrames(arg[i])
+		end
+	else
+		local _, _, _, enabled = GetAddOnInfo(addon)
+		if enabled then
+			addons[addon] = {}
+			for i = 1, arg.n do
+				tinsert(addons[addon], arg[i])
+			end
+		end
+	end
+end
+
+_G.Aero = Aero
---
+++ Frames.lua
@@ -0,0 +1,79 @@
+﻿--[[
+Aero:RegisterFrames(...)
+Use it to register show/hide animation to frames
+Arguments can be either frame names or hook functions, seperated by commas
+]]
+
+Aero:RegisterFrames(
+	"GameMenuFrame",
+	"SoundOptionsFrame",
+	"OptionsFrame",
+	"UIOptionsFrame",
+	"OpacityFrame",
+	"ItemRefTooltip",
+	"OpenMailFrame",
+	"StackSplitFrame",
+	"ColorPickerFrame",
+	"GuildInfoFrame",
+	"ReputationDetailFrame",
+	"LootFrame",
+	"HelpFrame",
+	"MailFrame",
+	"TradeFrame",
+	"GossipFrame",
+	"TabardFrame",
+	"FriendsFrame",
+	"MerchantFrame",
+	"QuestLogFrame",
+	"PetStableFrame",
+	"BattlefieldFrame",
+	"CharacterFrame",
+	"BankFrame"
+)
+
+local orig = {}
+
+Aero:RegisterFrames"SpellBookFrame"
+orig.ToggleSpellBook = ToggleSpellBook
+function ToggleSpellBook(bookType)
+	local bt = SpellBookFrame.bookType
+	if not this then this = SpellBookFrame end
+	orig.ToggleSpellBook(bookType)
+	if SpellBookFrame.hiding and bt ~= SpellBookFrame.bookType then
+		HideUIPanel(SpellBookFrame)
+		ShowUIPanel(SpellBookFrame)
+		SpellBookFrame_OnShow()
+		SpellBookFrame.onfinishhide = nil
+		SpellBookFrame.hiding = nil
+	end
+end
+
+Aero:RegisterFrames"WorldMapFrame"
+orig.WorldMapButton_OnUpdate = WorldMapButton_OnUpdate
+function WorldMapButton_OnUpdate(elapsed)
+	if not this:GetCenter() then return end
+	orig.WorldMapButton_OnUpdate(elapsed)
+end
+BlackoutWorld:SetAllPoints(WorldMapFrame)
+
+--[[
+Aero:RegisterAddon(addon, ...)
+Use it to register frames that are created after addon loaded or on demand
+Arguments:
+	addon - addon's name
+	... - either frame names or hook functions, seperated by commas
+]]
+
+Aero:RegisterAddon("Blizzard_CraftUI", "CraftFrame")
+Aero:RegisterAddon("Blizzard_BindingUI", "KeyBindingFrame")
+Aero:RegisterAddon("Blizzard_MacroUI", "MacroFrame")
+Aero:RegisterAddon("Blizzard_AuctionUI", "AuctionFrame", "AuctionDressUpFrame")
+Aero:RegisterAddon("Blizzard_GuildBankUI", "GuildBankFrame")
+Aero:RegisterAddon("Blizzard_TalentUI", "TalentFrame")
+Aero:RegisterAddon("Blizzard_TradeSkillUI", "TradeSkillFrame")
+Aero:RegisterAddon("Blizzard_TrainerUI", "ClassTrainerFrame")
+Aero:RegisterAddon("Blizzard_GMSurveyUI", "GMSurveyFrame")
+Aero:RegisterAddon("Blizzard_AchievementUI", "AchievementFrame")
+Aero:RegisterAddon("Blizzard_BattlefieldMinimap", "BattlefieldMinimap")
+Aero:RegisterAddon("Blizzard_ItemSocketingUI", "ItemSocketingFrame")
+Aero:RegisterAddon("TimeManager", "TimeManagerFrame")
